7ad5b641e50e7b272e0270026a26f517b39d2846
Vraiment <jemc44@gmail.com>
2017-08-12 11:54:42 -0700

    Added EventUtils skeleton

---
+++ tests/test_eventutils.cc
@@ -0,0 +1,12 @@
+#include <SDL.h>
+
+#include <SDL2pp/SDL.hh>
+#include <SDL2pp/EventUtils.hh>
+
+#include "testing.h"
+
+using namespace SDL2pp;
+using namespace SDL2pp::Event;
+
+BEGIN_TEST(int, char*[])
+END_TEST()
